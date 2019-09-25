b1afa1ff08777ae9a275bfe4e41f1e3fde17d22c
unknown <TULIL@UKOFFICE2.CAMARKETING.LOCAL>
2019-09-25 17:28:57 +0100

    not much

--- src/app/contact.ts
+++ src/app/contact.ts
@@ -1,2 +1,9 @@
 export interface Contact {
+    name: string;
+    phone_number: string;
+    address: string;
+    index_country: string;
+    index_post_code: string;
+    index_city: string;
+    index_house_number?: number;
 }
--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Contact } from './contact';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+
+  contacts: Contact[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getContacts()  {
+    this.http.get<Contact[]>('http://www.mocky.io/v2/581335f71000004204abaf83').subscribe(contacts => {
+      contacts.map(contact => this.contacts.push(contact));
+    });
+    return this.contacts;
+  }
+
+  editContact(contact: Contact) {
+    this.contacts.
+  }
+
+  deleteContact(phone_number: string) {
+
+  }
 }
